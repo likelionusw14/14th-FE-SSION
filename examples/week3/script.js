51b57fd3bcf3237032d22907e07ce48756d736a2
som <133498102+leesomin7747@users.noreply.github.com>
2026-04-05 01:42:28 +0900

    Update script.js

--- examples/week3/script.js
+++ examples/week3/script.js
@@ -1,1 +1,15 @@
+let input = document.querySelector("#input");
+let addBtn = document.querySelector("#addBtn");
+let list = document.querySelector("#list");
 
+let todos = [];
+
+addBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+
+}
+
+function render() {
+
+}
