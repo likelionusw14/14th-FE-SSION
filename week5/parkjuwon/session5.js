4592961228bddc489e2b74c73e1b4c55c980bfad
qkrwndnjs2640@naver.com <qkrwndnjs2640@naver.com>
2026-05-03 00:41:21 +0900

    feat: week5 Session 과제 제출 - 박주원

--- week5/parkjuwon/session5.js
+++ week5/parkjuwon/session5.js
@@ -1,101 +1,58 @@
-//문제 1
+//문제 1 두 수의 합 구하기
 
 function solution(num1, num2) {
-    if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100) {
-        const result = num1 * num2;
-        return result;
+    if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000){
+       return num1 + num2;    
+       }
+}
+
+//문제2 나머지 구하기
+
+function solution(num1, num2) {
+    if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100 )
+    return (num1%num2);
+}
+
+//문제3 분수의 덧셈
+
+function solution(numer1, denom1, numer2, denom2) {
+    let top = (numer1 * denom2) + (numer2 * denom1);
+    let bottom = denom1 * denom2;
+
+    let maximum = 0;
+    for (let i = 1; i <= top; i++) {
+        if (top % i === 0 && bottom % i === 0) {
+            maximum = i;
+        }
     }
+
+    return [top / maximum, bottom / maximum];
 }
 
-//문제2
-
-function solution(num1, num2) {
-    if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100) {
-        const result = Math.floor((num1 / num2) * 1000);
-        return result;
-    }
-}
-
-//문제3
-
-function solution(num1, num2) {
-    if (0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000) {
-        return num1 === num2 ? 1 : -1;
-    }
-}
-
-//문제4
-
-function solution(num1, num2) {
-    if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100)
-        return (num1 % num2);
-}
-
-//문제5
-
-function solution(age) {
-    if (0 < age && age <= 120) {
-        return (2023 - age);
-    }
-}
-
-//문제6
-
-function solution(num1, num2) {
-    if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000)
-        return (num1 - num2);
-}
-
-//문제7
-
-function solution(num1, num2) {
-    if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100)
-        return Math.floor(num1 / num2);
-}
-
-//문제8
-
-function solution(array, height) {
-    if (array.length < 1 || array.length > 100) return 0;
-    if (height < 1 || height > 200) return 0;
-
-    const tall = array.filter(function (item) {
-        return (item >= 1 && item <= 200) && (item > height);
-    });
-
-    return tall.length
-}
-
-//문제9
-
-function solution(num1, num2) {
-    if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
-        return num1 + num2;
-    }
-}
-
-//문제10
+//문제4 짝수는 싫어요
 
 function solution(n) {
-    if (1 <= n && n <= 100) {
-        return Math.floor((n + 6) / 7);
-    }
-}
-
-//문제11
-
-function solution(slice, n) {
-    var answer = 0;
-    answer = Math.floor(n / slice);
-    if (n % slice != 0) {
-        answer += 1;
+    var answer = [];
+    for (let i = 1; i <= n; i++){
+        if (i % 2 !== 0){
+            answer.push(i);
+        }
     }
     return answer;
 }
 
-//문제12
+
+//문제5 피자 나눠먹기(1)
+
+function solution(n) {
+    if (1 <= n && n <= 100){
+        return Math.floor((n+6) / 7);
+    }
+}
+
+//문제6 배열의 평균값
 
 function solution(numbers) {
     const sum = numbers.reduce((acc, cur) => acc + cur, 0);
-    return (sum / numbers.length);
+    return (sum/numbers.length);
 }
