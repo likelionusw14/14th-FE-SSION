d724778ebe865d6cc56bae293fa8667d4b182de7
율도 최 <yuldo04@gmail.com>
2026-05-10 20:43:04 +0900

    feat: week{6} {PBL or Session} 과제 제출 - 최율도

--- week6/choiyuldo/Session/src/MenuItem.jsx
+++ week6/choiyuldo/Session/src/MenuItem.jsx
@@ -4,19 +4,33 @@
 // TODO: props 3개를 받아야 합니다.
 //       name(메뉴이름), price(가격), emoji(이모지)
 
-function MenuItem({ }) {
+function MenuItem({ name,price,emoji,soldOut}) {
+  let out;
+  if (soldOut){
+    out = <span className = "menu-soldout">품절</span>
+
+  }
+  else{
+    out = <span className = "menu-price">{price}</span>
+  }
   return (
     <div className="menu-item">
       {/* TODO: emoji를 표시하세요 */}
-      <p > </p>
+      <p className = "menu-emoji" >{emoji}</p>
 
       {/* TODO: name을 표시하세요 */}
-      <h3 > </h3>
+      <h3 className = "menu-name" > {name}</h3>
 
       {/* TODO: price를 표시하세요 */}
-      <p> </p>
+      {out}
+      {/* {soldOut ? (
+        <span className = "menu-soldout">품절</span>
+      ):(
+        <p className = "menu-price">{price}</p>
+      )} */}
     </div>
   )
 }
+
 
 export default MenuItem
