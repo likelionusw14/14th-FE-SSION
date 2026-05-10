f1ee4674cbdb06ac5b453a6c641692d91e7f8201
Kim Minyoung <162345152+mi-noong@users.noreply.github.com>
2026-05-11 04:31:42 +0900

    Add files via upload

---
+++ examples/week6/ex05/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>input 포커스</h1>
+
+      <input 
+        type="text" 
+        placeholder="이름 입력"
+         />
+
+      <button>포커스 이동</button>
+    </div>
+  )
+}
+
+export default App
